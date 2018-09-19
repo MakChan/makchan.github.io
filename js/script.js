6087c9e07ceb2553ffeaf92e426b9d3843afa302
Mayank Chauhan <mayank.chauhan15@gmail.com>
2018-09-20 02:15:14 +0530

    Change project structure Add new content

--- js/script.js
+++
@@ -1,6 +0,0 @@
-
-$(".navigation-mobile a").click(function() {
-  console.log("hey");
-  $("div.mdl-layout__drawer").removeClass("is-visible");
-  $("div.mdl-layout__obfuscator").removeClass("is-visible");
-});
